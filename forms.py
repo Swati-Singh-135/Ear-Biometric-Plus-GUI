8ef044c3ba04cadbd8c1042061ef8a413e52b065
Swati Singh <swatipragya2010@gmail.com>
2024-08-15 17:07:23 +0530

    Form added in new file

--- forms.py
+++ forms.py
@@ -1,12 +1,54 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FileField
+from wtforms import StringField, SubmitField, FileField, IntegerField, validators, DateField, RadioField, SelectField
+from flask_wtf.file import FileRequired, FileAllowed
+from flask_uploads import UploadSet, IMAGES
 
-class Register(FlaskForm):
-    faceImage = FileField(
+
+photos = UploadSet('photos',IMAGES)
+
+
+class RegForm(FlaskForm):
+    photo = FileField(
+        label="Profile Image",
         validators=[
-            FileAllowed()
+            FileAllowed(photos,'Only images are allowed'),
+            FileRequired('Profile Image Field should not be empty')
         ]
     )
-    earImage = PasswordField('Ear Image')
-    name = StringField('Name')
-    submit = SubmitField('Sign up')
+    earphoto = FileField(
+        label="Ear Image",
+        validators=[
+            FileAllowed(photos,'Only images are allowed'),
+            FileRequired('Ear Image Field should not be empty')
+        ]
+    )
+    name = StringField(
+        label="Name",
+        validators=[
+            validators.InputRequired('Please enter the name')
+        ]
+    )
+    regno = IntegerField(
+        validators=[
+            validators.InputRequired('Please enter the registration number')
+        ]
+    )
+    fathername = StringField(
+        label="Father Name",
+        validators=[
+            validators.InputRequired('Please enter the father\'s name')
+        ]
+    )
+    dob = DateField(
+        label="DOB",
+        validators=[
+            validators.InputRequired('Please enter the date of birth')
+        ]
+    )
+    bloodgroup = StringField(
+        label="Blood Group",
+        validators=[
+            validators.InputRequired('Please enter the blood group')
+        ]
+    )
+    submit = SubmitField('Submit')
--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 import os
 from flask import Flask, render_template, send_from_directory, url_for
-# from forms import Register
+from forms import RegForm
 from flask_uploads import UploadSet, IMAGES, configure_uploads
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FileField, IntegerField, validators, DateField, RadioField, SelectField
-from flask_wtf.file import FileRequired, FileAllowed
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'B3NzaC1ycDAQABAAABAQCkPsx7jEchCJDX'
@@ -13,51 +12,6 @@ app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
 photos = UploadSet('photos',IMAGES)
 configure_uploads(app,photos)
 
-class UploadForm(FlaskForm):
-    photo = FileField(
-        label="Profile Image",
-        validators=[
-            FileAllowed(photos,'Only images are allowed'),
-            FileRequired('Profile Image Field should not be empty')
-        ]
-    )
-    earphoto = FileField(
-        label="Ear Image",
-        validators=[
-            FileAllowed(photos,'Only images are allowed'),
-            FileRequired('Ear Image Field should not be empty')
-        ]
-    )
-    name = StringField(
-        label="Name",
-        validators=[
-            validators.InputRequired('Please enter the name')
-        ]
-    )
-    regno = IntegerField(
-        validators=[
-            validators.InputRequired('Please enter the registration number')
-        ]
-    )
-    fathername = StringField(
-        label="Father Name",
-        validators=[
-            validators.InputRequired('Please enter the father\'s name')
-        ]
-    )
-    dob = DateField(
-        label="DOB",
-        validators=[
-            validators.InputRequired('Please enter the date of birth')
-        ]
-    )
-    bloodgroup = StringField(
-        label="Blood Group",
-        validators=[
-            validators.InputRequired('Please enter the blood group')
-        ]
-    )
-    submit = SubmitField('Submit')
 
 @app.route('/uploads/<filename>')
 def get_file(filename):
@@ -66,7 +20,7 @@ def get_file(filename):
 
 @app.route('/register', methods=['GET','POST'])
 def upload_image():
-    form = UploadForm()
+    form = RegForm()
     if form.validate_on_submit():
         filename = photos.save(form.photo.data)
         profile_url = url_for('get_file',filename=filename)
@@ -82,7 +36,7 @@ def upload_image():
         profile_url = None
         ear_url = None
 
-    return render_template('signup.html',form=form, profile_url=profile_url, ear_url=ear_url)
+    return render_template('registration.html',form=form, profile_url=profile_url, ear_url=ear_url)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
